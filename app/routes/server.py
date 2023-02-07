99624ef1dd3231e64051e20d2ab3a99b994e8f11
Geiselle Holt <geiselleholt@hotmail.com>
2023-02-06 22:42:09 -0700

    changed product5

--- app/routes/server.py
+++ app/routes/server.py
@@ -37,7 +37,7 @@ def create_checkout_session():
                     'quantity': 1,
                 },
                 {
-                    'price': 'price_1MXBRQDxl1uhmuIn2IB2h6TR',
+                    'price': 'price_1MYjvzDxl1uhmuIn9JEruSqc',
                     "adjustable_quantity": {"enabled": True, "minimum": 1, "maximum": 10},
                     'quantity': 1,
                 },
