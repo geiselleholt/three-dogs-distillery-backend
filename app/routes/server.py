32304580c070af5274b38ea440678f7e8087dac9
Geiselle Holt <geiselleholt@hotmail.com>
2023-02-07 19:23:08 -0600

    thankyou and cancel url

--- app/routes/server.py
+++ app/routes/server.py
@@ -54,8 +54,8 @@ def create_checkout_session():
             ],
             mode='payment',
             # success_url="https://three-dogs-distillery-backend.herokuapp.com/order/success?session_id={CHECKOUT_SESSION_ID}",
-            success_url="https://three-dogs-distillery.herokuapp.com/thankyou",
-            cancel_url="https://three-dogs-distillery.herokuapp.com/cancelorder",
+            success_url="http://localhost:3000/thankyou",
+            cancel_url="http://localhost:3000/cancelorder",
             automatic_tax={'enabled': True},
         )
     
